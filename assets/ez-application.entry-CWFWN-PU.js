383c2ba3d062afd579aae0e580090568ee90a5ec
israel.moura <israel.moura@sankhya.com.br>
2025-05-07 10:10:21 -0300

    Instalacao com DS 2

---
+++ assets/ez-application.entry-CWFWN-PU.js
@@ -0,0 +1,2 @@
+import{b as a,d as i,e as o}from"./index-BCuik2gT.js";const e="",n=class{constructor(t){a(this,t),this.applicationLoaded=i(this,"applicationLoaded")}componentWillLoad(){}componentDidLoad(){this.applicationLoaded.emit(!0)}render(){return o("div",null)}};n.style=e;export{n as ez_application};
+//# sourceMappingURL=ez-application.entry-CWFWN-PU.js.map
